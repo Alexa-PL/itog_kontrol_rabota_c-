d41107dee013a91a5962c3672859b14fbe6166ac
Aleksandra PLatanaya <platanaya48@gmail.com>
2024-06-02 20:18:27 +0300

    Ввод от пользователя

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,4 +9,53 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Устанавливаем кодировку ввода на UTF-16 чтобы работало с кирилицей
+        Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+
+        System.Console.WriteLine("Введите строку из слов, разделенных пробелами:");
+        string input = Console.ReadLine(); // Получаем ввод от пользователя
+
+
+        PrintArray(originalArray); // Печать оригинального массива
+        PrintArray(newArray); // Печать нового массива
+
+
+        //
+        // Функции
+        //
+
+        void PrintArray(string[] array) //Функция печати массива
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.Write(array[i] + " ");
+            }
+            System.Console.WriteLine();
+        }
+
+        string[] CreateArrayMaxThreeChars(string[] array) //Функция создания нового массива где элементы не длиннее 3 символов
+        {
+            string[] newArray = new string[originalArray.Length]; // Создаем новый массив такой же длины, как оригинальный
+            int newIndex = 0; // Инициализируем индекс для нового массива
+
+            // Формирование нового массива
+            foreach (string str in originalArray) // Перебираем каждый элемент оригинального массива
+            {
+                // Если длина строки <= 3 символов, добавляем в новый массив
+                if (str.Length <= 3) // Проверяем длину текущей строки
+                {
+                    newArray[newIndex] = str; // Добавляем строку в новый массив
+                    newIndex++; // Увеличиваем индекс нового массива для следующего элемента
+                }
+            }
+            return newArray; //возвращаем новый массив
+        }
+    }
+
+}
